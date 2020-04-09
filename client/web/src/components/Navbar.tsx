95ebed4f4f471f49d9ea1bee612119629fccea7a
bat <b>
2020-04-09 13:53:39 +0200

    Add Version to the Ui Nav bar
    Get the version from package.json and display it

--- client/web/src/components/Navbar.tsx
+++ client/web/src/components/Navbar.tsx
@@ -1,20 +1,27 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-export const Navbar: React.FC = () => (
-  <nav>
-    <div className="nav-wrapper cyan darken-1 px1">
-      <NavLink to="/" className="brand-logo">
-        Redux + TypeScript
-      </NavLink>
-      <ul className="right hide-on-med-and-down">
-        <li cy-data="home-nav-link">
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/about">About</NavLink>
-        </li>
-      </ul>
-    </div>
-  </nav>
-)
+export const Navbar: React.FC = () => 
+{
+  let pjson = require('../../package.json');
+  let version = pjson.version;
+
+  return (
+    <nav>
+      <div className="nav-wrapper cyan darken-1 px1">
+        <NavLink to="/" className="brand-logo">
+          Health-Barbecue
+        </NavLink>
+        <div  className="version" >{version}</div>
+        <ul className="right hide-on-med-and-down">
+          <li cy-data="home-nav-link">
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
