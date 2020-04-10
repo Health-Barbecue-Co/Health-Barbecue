92824fb1c96f875a2f0c5ed178c5ea40615597f4
Damien Suhard <damien.suhard@oveasoft.com>
2020-04-10 09:21:35 +0200

    WEB - Test proxy with Studies List component

--- client/web/src/components/studies/list/StudiesList.tsx
+++ client/web/src/components/studies/list/StudiesList.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 type StudiesListProps = {}
 
 export const StudiesList: React.FC<StudiesListProps> = () => {
-	return <div>StudiesList</div>
+	let list: any = [];
+
+	useEffect(() => {
+		fetch('/api/studies').then((content: Response) => {
+			console.log('__________________', content)
+			list = content.body
+		})
+	}, [])
+	return (
+		<>
+			<div>StudiesList</div>
+			<div>{JSON.stringify(list)}</div>
+		</>
+	)
 }
