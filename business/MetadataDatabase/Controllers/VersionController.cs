bda5a33dd243300403513f807cc463e842948c85
Nalien <thenalien@gmail.com>
2020-04-16 15:03:54 +0200

    Add version API. Displayed version on UI.

---
+++ business/MetadataDatabase/Controllers/VersionController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MetadataDatabase.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        /// <summary>
+        /// Gets the Assembly version.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<Version> Get()
+        {
+            var version = typeof(VersionController).Assembly.GetName().Version;
+            return this.Ok(version);
+        }
+
+        /// <summary>
+        /// Gets the Assembly version.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("api")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<String> GetApiVersion()
+        {
+            var version = "1.0"; // TODO use global API versioning
+            return this.Ok(version);
+        }
+
+    }
+}
