a204ba8c56eae46b5c16ba0a33ad4f1cb73f7e5e
bat3 <saccomandi.baptiste@gmail.com>
2020-04-18 18:11:23 +0200

    Update UI to synchronize pacs and BE

--- client/web/src/components/series/list/SeriesList.tsx
+++ client/web/src/components/series/list/SeriesList.tsx
@@ -9,7 +9,11 @@ import {
   TableCell,
   TableBody,
   makeStyles,
+  Button,
+  Grid,
 } from '@material-ui/core'
+import SyncIcon from '@material-ui/icons/Sync'
+import { useTranslation } from 'react-i18next'
 
 import styles from './SeriesList.style'
 
@@ -20,34 +24,52 @@ type SeriesListProps = {}
 export const SeriesList: React.FC<SeriesListProps> = () => {
   const [list, setList] = useState([])
   const classes = useStyles()
+  const { t } = useTranslation()
 
-  useEffect(() => {
+  const updateSeriesList = () => {
     axios.get('/api/series').then((response) => {
       setList(response.data)
     })
+  }
+
+  const refresh = () => {
+    axios.get('/api/PacsMirror').then(() => {
+      updateSeriesList();
+    });
+  }
+
+  useEffect(() => {
+    updateSeriesList();
   }, [])
 
   return (
     <TableContainer component={Paper}>
+      <Grid container justify="flex-end">
+        <Button 
+          variant="contained"
+          onClick={() => refresh()}
+          startIcon={<SyncIcon />}>{t('Refresh')}
+        </Button>
+      </Grid>
       <Table className={classes.table} aria-label="simple table">
         <TableHead>
           <TableRow>
             <TableCell>seriesInstanceUID</TableCell>
-            <TableCell align="right">title 2</TableCell>
-            <TableCell align="right">title 3</TableCell>
-            <TableCell align="right">title 4</TableCell>
-            <TableCell align="right">title 5</TableCell>
+            <TableCell align="right">Series description</TableCell>
+            <TableCell align="right">Modality</TableCell>
+            <TableCell align="right">Number Of Instances</TableCell>
+            <TableCell align="right">Body part</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {list.map(({ id, seriesInstanceUID }) => (
+          {list.map(({ id, seriesInstanceUID, seriesDescription, modality, numberOfSeriesRelatedInstances }) => (
             <TableRow key={id}>
               <TableCell component="th" scope="row">
                 {seriesInstanceUID}
               </TableCell>
-              <TableCell align="right">---</TableCell>
-              <TableCell align="right">---</TableCell>
-              <TableCell align="right">---</TableCell>
+              <TableCell align="right">{seriesDescription}</TableCell>
+              <TableCell align="right">{modality}</TableCell>
+              <TableCell align="right">{numberOfSeriesRelatedInstances}</TableCell>
               <TableCell align="right">---</TableCell>
             </TableRow>
           ))}
