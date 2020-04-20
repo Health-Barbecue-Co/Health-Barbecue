333732d5aea6556c78422beb5ec64be51cd041ed
bat3 <saccomandi.baptiste@gmail.com>
2020-04-20 18:01:42 +0200

    Fix develop merge
    Update UI part with new redux saga implementation

--- client/web/src/components/series/list/SeriesList.tsx
+++ client/web/src/components/series/list/SeriesList.tsx
@@ -13,11 +13,11 @@ import {
 } from '@material-ui/core'
 import SyncIcon from '@material-ui/icons/Sync'
 import { useTranslation } from 'react-i18next'
-
 import { useDispatch, useSelector } from 'react-redux'
 
 import { actionTypes, selectors } from '../../../features/series'
 import styles from './SeriesList.style'
+import { HttpRequest } from '../../../features/common/HttpRequest'
 
 const useStyles = makeStyles(styles)
 
@@ -25,7 +25,7 @@ type SeriesListProps = {}
 
 export const SeriesList: React.FC<SeriesListProps> = () => {
   const { list } = useSelector(selectors.getSeriesStore)
-
+  const { t } = useTranslation()
   const classes = useStyles()
   const dispatch = useDispatch()
 
@@ -33,12 +33,19 @@ export const SeriesList: React.FC<SeriesListProps> = () => {
     dispatch({ type: actionTypes.FETCH_ALL_SERIES })
   }, [dispatch])
 
+  const synchronize = () => {
+    let httpRequest = new HttpRequest({ baseURL: '/api' })
+    httpRequest.get("/PacsMirror").then(()=> {
+      dispatch({ type: actionTypes.FETCH_ALL_SERIES })
+    });
+  }
+
   return (
     <TableContainer component={Paper}>
       <Grid container justify="flex-end">
         <Button 
           variant="contained"
-          onClick={() => refresh()}
+          onClick={() => synchronize()}
           startIcon={<SyncIcon />}>{t('Refresh')}
         </Button>
       </Grid>
@@ -54,14 +61,14 @@ export const SeriesList: React.FC<SeriesListProps> = () => {
         </TableHead>
         <TableBody>
           {list &&
-            list.map(({ id, seriesInstanceUID }) => (
+            list.map(({ id, seriesInstanceUID, seriesDescription, modality, numberOfSeriesRelatedInstances }) => (
               <TableRow key={id}>
                 <TableCell component="th" scope="row">
                   {seriesInstanceUID}
                 </TableCell>
-                <TableCell align="right">---</TableCell>
-                <TableCell align="right">---</TableCell>
-                <TableCell align="right">---</TableCell>
+                <TableCell align="right">{seriesDescription}</TableCell>
+                <TableCell align="right">{modality}</TableCell>
+                <TableCell align="right">{numberOfSeriesRelatedInstances}</TableCell>
                 <TableCell align="right">---</TableCell>
               </TableRow>
             ))}
