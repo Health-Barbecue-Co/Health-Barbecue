81e74197c810a80518545be66657ea02e075c5b8
Cyril <c.quennouelle@quantumsurgical.com>
2020-04-20 17:56:52 +0200

    Test GetSeries, DeleteSeries, GetSeriesId.

--- integtests/IntegTests/BusinessApiTest.cs
+++ integtests/IntegTests/BusinessApiTest.cs
@@ -2,23 +2,79 @@ using System;
 using System.Net;
 using Xunit;
 using RestSharp;
-using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace IntegTests
 {
-    public class BusinessApiTest
+    public class BusinessApiTest : IDisposable
     {
+        private RestClient client = new RestClient("http://localhost:5000");
+        private RestRequest getSeriesRequest = new RestRequest("/api/Series", DataFormat.Json);
+
         [Fact]
         public void GetSeries()
         {
-            var client = new RestClient("http://localhost:5000");
-            //client.Authenticator = new HttpBasicAuthenticator("username", "password");
-
-            var request = new RestRequest("/api/Series", DataFormat.Json);
-
-            var response = client.Get(request);
-
+            var response = client.Get(getSeriesRequest);
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         }
+        
+        [Fact]
+        public void PostSeries()
+        {
+            var postSeriesRequest = new RestRequest("/api/Series", DataFormat.Json);
+            postSeriesRequest.AddJsonBody(new { SeriesInstanceUID="TestPostSeries" });
+
+            var response = client.Post(postSeriesRequest);
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+            string newId = response.Content;
+
+            var getSeriesRequest = new RestRequest("/api/Series", DataFormat.Json);
+            response = client.Get(getSeriesRequest);
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            string responseString = response.Content;
+            Assert.Contains(newId, responseString);
+        }
+
+        [Fact]
+        public void DeleteSeries()
+        {
+            var postSeriesRequest = new RestRequest("/api/Series", DataFormat.Json);
+            postSeriesRequest.AddJsonBody(new { SeriesInstanceUID = "TestDeleteSeries" });
+
+            var response = client.Post(postSeriesRequest);
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+            HBSeries series = JsonConvert.DeserializeObject<HBSeries>(response.Content);
+
+            var deleteSeriesTotoRequest = new RestRequest("/api/Series/" + "Toto", DataFormat.Json);
+            response = client.Delete(deleteSeriesTotoRequest);
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+
+            var deleteSeriesRequest = new RestRequest("/api/Series/" + series.Id, DataFormat.Json);
+            response = client.Delete(deleteSeriesRequest);
+            Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+
+        }
+ 
+        private void deleteAllSeries()
+        {
+            var response = client.Get(getSeriesRequest);
+            string responseString = response.Content;
+            List<HBSeries> seriesList = JsonConvert.DeserializeObject<List<HBSeries>>(responseString);
+
+            foreach (var series in seriesList)
+            {
+                var deleteSeriesRequest = new RestRequest("/api/Series/" + series.Id, DataFormat.Json);
+                response = client.Delete(deleteSeriesRequest);
+            }
+        }
+
+        public void Dispose()
+        {
+            deleteAllSeries();
+        }
+
     }
 }
---
+++ integtests/IntegTests/HBSeries.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntegTests
+{
+    /// <summary>
+    /// Class representing a Series
+    /// </summary>
+    class HBSeries
+    {
+        public string Id { get; set; }
+        public string SeriesInstanceUID { get; set; }
+    }
+}
