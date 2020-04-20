23ffd4aaf9d742591f38126deadef448613e4059
Damien Suhard <damien.suhard@oveasoft.com>
2020-04-20 23:49:00 +0200

    Frontend : User form

---
+++ client/web/src/components/users/Register/RegisterUser.style.ts
@@ -0,0 +1,18 @@
+import { Theme, createStyles } from '@material-ui/core'
+
+export default (theme: Theme) =>
+  createStyles({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  })
--- client/web/src/components/users/Register/RegisterUser.tsx
+++ client/web/src/components/users/Register/RegisterUser.tsx
@@ -1,3 +1,155 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useFormik } from 'formik'
+import { useTranslation } from 'react-i18next'
+// import { useDispatch } from 'react-redux'
+import {
+  Box,
+  TextField,
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+  InputAdornment,
+  IconButton,
+  Button,
+  makeStyles,
+  Container,
+  CssBaseline,
+} from '@material-ui/core'
 
-export const UserRegister: React.FC = () => <div>Register User</div>
+import Visibility from '@material-ui/icons/Visibility'
+import VisibilityOff from '@material-ui/icons/VisibilityOff'
+
+import style from './RegisterUser.style'
+
+type UserRegisterProps = {}
+const useStyle = makeStyles(style)
+
+export const UserRegister: React.FC<UserRegisterProps> = () => {
+  const { t } = useTranslation()
+  const classes = useStyle()
+  // const dispatch = useDispatch()
+  const [showPassword, setShowPassword] = useState(false)
+
+  const formik = useFormik({
+    initialValues: {
+      firstname: '',
+      lastname: '',
+      login: '',
+      password: '',
+      role: 'admin',
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2))
+    },
+  })
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword)
+  }
+
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault()
+  }
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <form onSubmit={formik.handleSubmit}>
+          <Box display="flex" flexDirection="row" p={1}>
+            <Box width={1 / 2} pr={1}>
+              <TextField
+                error
+                id="firstname"
+                label={t('user:field.firstname')}
+                defaultValue={formik.values.firstname}
+                variant="outlined"
+                required
+                helperText={formik.errors.firstname}
+                onChange={formik.handleChange}
+              />
+            </Box>
+            <Box width={1 / 2} pl={1}>
+              <TextField
+                error
+                id="lastname"
+                label={t('user:field.lastname')}
+                defaultValue={formik.values.lastname}
+                variant="outlined"
+                required
+                helperText={formik.errors.lastname}
+                onChange={formik.handleChange}
+              />
+            </Box>
+          </Box>
+          <Box p={1}>
+            <TextField
+              error
+              fullWidth
+              id="login"
+              label={t('user:field.login')}
+              defaultValue={formik.values.login}
+              variant="outlined"
+              required
+              helperText={formik.errors.login}
+              onChange={formik.handleChange}
+            />
+          </Box>
+          <Box p={1}>
+            <FormControl variant="outlined" fullWidth>
+              <InputLabel htmlFor="password">
+                {t('user:field.password')}
+              </InputLabel>
+              <OutlinedInput
+                id="password"
+                fullWidth
+                type={showPassword ? 'text' : 'password'}
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                labelWidth={70}
+              />
+            </FormControl>
+          </Box>
+          <Box p={1}>
+            <TextField
+              error
+              fullWidth
+              id="role"
+              label={t('user:field.role')}
+              defaultValue={formik.values.role}
+              variant="outlined"
+              required
+              helperText={formik.errors.role}
+              onChange={formik.handleChange}
+            />
+          </Box>
+          <Box p={1}>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              {t('action.save')}
+            </Button>
+          </Box>
+        </form>
+      </div>
+    </Container>
+  )
+}
