4bf289fb27557439d98d2f7e457932ef18749950
bat3 <saccomandi.baptiste@gmail.com>
2020-04-22 18:51:16 +0200

    Delete unused import

--- client/web/src/components/series/list/SeriesList.tsx
+++ client/web/src/components/series/list/SeriesList.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react'
-import { makeStyles } from '@material-ui/core'
 import SyncIcon from '@material-ui/icons/Sync'
 import { useTranslation } from 'react-i18next'
 import { useDispatch, useSelector } from 'react-redux'
