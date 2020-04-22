0e01d91ce5a3440d099ed965a9a3bb54e7e65e83
bat3 <saccomandi.baptiste@gmail.com>
2020-04-22 18:47:22 +0200

    Updates UI series list and nav bar
    Updates series list with filter and pagination
    Updates nav bar with Projects and algorithms

--- client/web/src/components/series/list/SeriesList.tsx
+++ client/web/src/components/series/list/SeriesList.tsx
@@ -1,32 +1,18 @@
 import React, { useEffect } from 'react'
-import {
-  TableContainer,
-  Paper,
-  Table,
-  TableHead,
-  TableRow,
-  TableCell,
-  TableBody,
-  makeStyles,
-  Button,
-  Grid,
-} from '@material-ui/core'
+import { makeStyles } from '@material-ui/core'
 import SyncIcon from '@material-ui/icons/Sync'
 import { useTranslation } from 'react-i18next'
 import { useDispatch, useSelector } from 'react-redux'
+import MaterialTable from 'material-table'
 
 import { actionTypes, selectors } from '../../../features/series'
 import { mirrorPacsActionTypes } from '../../../features/mirrorPacs'
-import styles from './SeriesList.style'
-
-const useStyles = makeStyles(styles)
 
 type SeriesListProps = {}
 
 export const SeriesList: React.FC<SeriesListProps> = () => {
   const { list } = useSelector(selectors.getSeriesStore)
   const { t } = useTranslation()
-  const classes = useStyles()
   const dispatch = useDispatch()
 
   useEffect(() => {
@@ -38,39 +24,29 @@ export const SeriesList: React.FC<SeriesListProps> = () => {
   }
 
   return (
-    <TableContainer component={Paper}>
-      <Grid container justify="flex-end">
-        <Button 
-          variant="contained"
-          onClick={() => synchronize()}
-          startIcon={<SyncIcon />}>{t('Refresh')}
-        </Button>
-      </Grid>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>seriesInstanceUID</TableCell>
-            <TableCell align="right">Series description</TableCell>
-            <TableCell align="right">Modality</TableCell>
-            <TableCell align="right">Number Of Instances</TableCell>
-            <TableCell align="right">Body part</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {list &&
-            list.map(({ id, seriesInstanceUID, seriesDescription, modality, numberOfSeriesRelatedInstances }) => (
-              <TableRow key={id}>
-                <TableCell component="th" scope="row">
-                  {seriesInstanceUID}
-                </TableCell>
-                <TableCell align="right">{seriesDescription}</TableCell>
-                <TableCell align="right">{modality}</TableCell>
-                <TableCell align="right">{numberOfSeriesRelatedInstances}</TableCell>
-                <TableCell align="right">---</TableCell>
-              </TableRow>
-            ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <div style={{ maxWidth: '100%' }}>
+      <MaterialTable
+        title="Series list"
+        columns={[
+          { title: 'Series instance UID', field: 'seriesInstanceUID' },
+          { title: 'Series description', field: 'seriesDescription' },
+          { title: 'Modality', field: 'modality' },
+          { title: 'Number Of Instances', field: 'numberOfSeriesRelatedInstances' },
+          { title: 'Body part', field: 'birthCity' },
+        ]}
+        data={list}
+        options={{
+          filtering: true
+        }}
+        actions={[
+          {
+            icon: () => <SyncIcon />,
+            tooltip: t('Refresh'),
+            isFreeAction: true,
+            onClick: (event) => synchronize()
+          }
+        ]}
+      />
+    </div>
   )
 }
