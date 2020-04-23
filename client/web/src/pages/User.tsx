1aa420ca57970984e66665507dc7d8fa6bbbf421
Damien Suhard <damien.suhard@oveasoft.com>
2020-04-23 15:02:38 +0200

    Frontend : Fix Register access from signin page

--- client/web/src/pages/User.tsx
+++ client/web/src/pages/User.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
-import { Switch, useRouteMatch, Route, Redirect } from 'react-router-dom'
+import {
+  Switch,
+  useRouteMatch,
+  Route,
+  Redirect,
+  useHistory,
+} from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import {
   SignIn,
@@ -12,6 +18,7 @@ import { selectors } from '../features/auth'
 export const User: React.FC = () => {
   const match = useRouteMatch()
   const user = useSelector(selectors.getAuth)
+  const history = useHistory()
 
   return (
     <>
@@ -22,7 +29,14 @@ export const User: React.FC = () => {
           component={ForgottenPassword}
           exact
         />
-        <Route path={`${match.path}/register`} component={UserRegister} exact />
+
+        <Route path={`${match.path}/register`} exact>
+          <UserRegister
+            afterValidate={() => {
+              history.goBack()
+            }}
+          />
+        </Route>
 
         <Route path={`${match.path}/main`}>
           {!user ? <Redirect to="/user" /> : <UserMain />}
