5895fc7381b39572b7fdab457c21e6a3d7f1dacc
Damien Suhard <damien.suhard@oveasoft.com>
2020-05-12 21:57:39 +0200

    Fix code review

--- business/MetadataDatabase/Convertor/UserSettingsConvertor.cs
+++ business/MetadataDatabase/Convertor/UserSettingsConvertor.cs
@@ -5,23 +5,17 @@ using MongoDB.Bson;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace MetadataDatabase.Convertor
-{
-    public static class UserSettingsConvertor
-    {
-        public static UserSettingsDto ToUserSettingsDto(this UserSettings settings)
-        {
-            return new UserSettingsDto
-            {
-							theme = settings.theme.ToUserThemeDto()
+namespace MetadataDatabase.Convertor {
+    public static class UserSettingsConvertor {
+        public static UserSettingsDto ToUserSettingsDto(this UserSettings settings) {
+            return new UserSettingsDto {
+                theme = settings.theme.ToUserThemeDto()
             };
         }
 
-				public static UserSettings ToModel(this UserSettingsDto settingsDto)
-        {
-            return new UserSettings
-            {
-							theme = settingsDto.theme.ToModel()
+        public static UserSettings ToModel(this UserSettingsDto settingsDto) {
+            return new UserSettings {
+                theme = settingsDto.theme.ToModel()
             };
         }
     }
